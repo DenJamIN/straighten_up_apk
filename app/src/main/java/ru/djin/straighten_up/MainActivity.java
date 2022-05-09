5ebcb36eef683315fb464ca6c374a9fb5b64c0cb
lavrentiev-d <denlavre@mail.ru>
2022-05-09 14:25:51 +0300

    добавлены stories используя внешную библиотеку StoriesProgressView

--- app/src/main/java/ru/djin/straighten_up/MainActivity.java
+++ app/src/main/java/ru/djin/straighten_up/MainActivity.java
@@ -9,103 +9,195 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import jp.shts.android.storiesprogressview.StoriesProgressView;
 
-public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener  {
 
-    final String CHANNEL_ID = "notifyStraightenUp";
-    private TextView timeAM;
-    private TextView timePM;
-    private SeekBar seekBarStart;
-    private SeekBar seekBarEnd;
+ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener  {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        setTitle("Выпрями спину!");
+     final String CHANNEL_ID = "notifyStraightenUp";
 
-        createNotificationChannelIfNeeded();
+     private TextView amTime;
+     private TextView pmTime;
+     private TextView amLabel;
+     private TextView pmLabel;
 
-        Button notifyBtn = findViewById(R.id.startButton);
-        timeAM = (TextView)findViewById(R.id.timeAMLabel);
-        timePM = (TextView)findViewById(R.id.timePMLabel);
-        seekBarStart = (SeekBar)findViewById(R.id.seekBarStart);
-        seekBarEnd = (SeekBar)findViewById(R.id.seekBarEnd);
-        seekBarStart.setOnSeekBarChangeListener((SeekBar.OnSeekBarChangeListener) this);
-        seekBarEnd.setOnSeekBarChangeListener((SeekBar.OnSeekBarChangeListener) this);
+     private SeekBar seekBarStart;
+     private SeekBar seekBarEnd;
 
-        notifyBtn.setOnClickListener(view -> {
-            Toast.makeText(this, "Reminder Set!", Toast.LENGTH_SHORT).show();
+     private StoriesProgressView storiesProgressView;
 
-            Intent intent = new Intent(MainActivity.this, ReminderBroadcast.class);
-            @SuppressLint("UnspecifiedImmutableFlag") PendingIntent pendingIntent = PendingIntent.getBroadcast(
-                    MainActivity.this,
-                    0,
-                    intent,
-                    PendingIntent.FLAG_UPDATE_CURRENT
-            );
+     private ImageView imageView;
+     private ImageView betaToolsView;
 
-            AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+     private ImageButton storiesBtn;
+     private Button notifyBtn;
 
-            long timeAtButtonClick = System.currentTimeMillis();
-            long tenSecondsInMillis = 1000 * 10;
+     private int counterStories = 0;
 
-            alarmManager.set(
-                    AlarmManager.RTC_WAKEUP,
-                    timeAtButtonClick + tenSecondsInMillis,
-                    pendingIntent
-            );
+     private final int[] resources = new int[]{
+             R.drawable.warmupfirst,
+             R.drawable.warmupsecond
+     };
 
-        });
-    }
+     @SuppressLint("ClickableViewAccessibility")
+     @Override
+     protected void onCreate(Bundle savedInstanceState) {
+         super.onCreate(savedInstanceState);
+         setContentView(R.layout.activity_main);
+         setTitle("Выпрями спину!");
 
-    private void createNotificationChannelIfNeeded(){
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
-            String description = "Channel for notify";
+         createNotificationChannelIfNeeded();
 
-            NotificationChannel notificationChannel = new NotificationChannel(
-                    CHANNEL_ID,
-                    CHANNEL_ID,
-                    NotificationManager.IMPORTANCE_DEFAULT
-            );
-            notificationChannel.setDescription(description);
+         notifyBtn = findViewById(R.id.startButton);
+         storiesBtn = findViewById(R.id.showStoriesButton);
+         amTime = findViewById(R.id.timeAMLabel);
+         pmTime = findViewById(R.id.timePMLabel);
+         amLabel = findViewById(R.id.chooseTimeAMLabel);
+         pmLabel = findViewById(R.id.chooseTimePMLabel);
+         seekBarStart = findViewById(R.id.seekBarStart);
+         seekBarEnd = findViewById(R.id.seekBarEnd);
+         betaToolsView = findViewById(R.id.betaToolsImage);
+         imageView = findViewById(R.id.imageForStories);
+         storiesProgressView = findViewById(R.id.stories);
 
-            NotificationManager manager = getSystemService(NotificationManager.class);
-            manager.createNotificationChannel(notificationChannel);
-        }
-    }
+         seekBarStart.setOnSeekBarChangeListener(this);
+         seekBarEnd.setOnSeekBarChangeListener(this);
 
-    @Override
-    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-        timeAM.setText(getTimeProgressText(seekBarStart.getProgress()+1, true));
-        timePM.setText(getTimeProgressText(seekBarEnd.getProgress()+1, false));
-    }
+         notifyBtn.setOnClickListener(view -> {
+             Toast.makeText(this, "Reminder Set!", Toast.LENGTH_SHORT).show();
 
-    @Override
-    public void onStartTrackingTouch(SeekBar seekBar) {
+             Intent intent = new Intent(MainActivity.this, ReminderBroadcast.class);
+             @SuppressLint("UnspecifiedImmutableFlag") PendingIntent pendingIntent = PendingIntent.getBroadcast(
+                     MainActivity.this,
+                     0,
+                     intent,
+                     PendingIntent.FLAG_UPDATE_CURRENT
+             );
 
-    }
+             AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
 
-    @Override
-    public void onStopTrackingTouch(SeekBar seekBar) {
+             long timeAtButtonClick = System.currentTimeMillis();
+             long tenSecondsInMillis = 1000 * 10;
 
-    }
+             alarmManager.set(
+                     AlarmManager.RTC_WAKEUP,
+                     timeAtButtonClick + tenSecondsInMillis,
+                     pendingIntent
+             );
 
-    private String getTimeProgressText(int progress, boolean am){
-        if (am){
-            return String.valueOf(progress) + ":00 AM (" + String.valueOf(progress) + ":00)";
-        }
-        else{
-            return String.valueOf(progress) + ":00 PM (" + String.valueOf(progress+12) + ":00)";
-        }
-    }
-}
+         });
+
+
+
+         storiesBtn.setOnClickListener(view -> {
+             counterStories = 0;
+
+             storiesProgressView.setStoriesCount(resources.length);
+             storiesProgressView.setStoryDuration(10_000L);
+
+             imageView.setVisibility(View.VISIBLE);
+
+             amTime.setVisibility(View.INVISIBLE);
+             pmTime.setVisibility(View.INVISIBLE);
+             amLabel.setVisibility(View.INVISIBLE);
+             pmLabel.setVisibility(View.INVISIBLE);
+             seekBarStart.setVisibility(View.INVISIBLE);
+             seekBarEnd.setVisibility(View.INVISIBLE);
+             notifyBtn.setVisibility(View.INVISIBLE);
+             storiesBtn.setVisibility(View.INVISIBLE);
+             betaToolsView.setVisibility(View.INVISIBLE);
+
+             storiesProgressView.startStories();
+             imageView.setImageResource(resources[counterStories]);
+
+             storiesProgressView.setStoriesListener(new StoriesProgressView.StoriesListener() {
+                 @Override
+                 public void onNext() {
+                     imageView.setImageResource(resources[++counterStories]);
+                 }
+
+                 @Override
+                 public void onPrev() { }
+
+                 @Override
+                 public void onComplete() {
+                     Toast.makeText(MainActivity.this, "Истории закончились", Toast.LENGTH_SHORT).show();
+
+                     amTime.setVisibility(View.VISIBLE);
+                     pmTime.setVisibility(View.VISIBLE);
+                     amLabel.setVisibility(View.VISIBLE);
+                     pmLabel.setVisibility(View.VISIBLE);
+                     seekBarStart.setVisibility(View.VISIBLE);
+                     seekBarEnd.setVisibility(View.VISIBLE);
+                     notifyBtn.setVisibility(View.VISIBLE);
+                     storiesBtn.setVisibility(View.VISIBLE);
+                     betaToolsView.setVisibility(View.VISIBLE);
+
+                     imageView.setVisibility(View.INVISIBLE);
+                 }
+             });
+
+             //Не скипаются при повторном воспроизводится потому что current возвращает ту же пикчу, а не следующую
+             imageView.setOnClickListener(v -> {
+                 if(imageView.getVisibility()==View.VISIBLE)
+                    storiesProgressView.skip();
+             });
+
+         });
+     }
+
+     private void createNotificationChannelIfNeeded(){
+         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+             String description = "Channel for notify";
+
+             NotificationChannel notificationChannel = new NotificationChannel(
+                     CHANNEL_ID,
+                     CHANNEL_ID,
+                     NotificationManager.IMPORTANCE_DEFAULT
+             );
+
+             notificationChannel.setDescription(description);
+
+             NotificationManager manager = getSystemService(NotificationManager.class);
+             manager.createNotificationChannel(notificationChannel);
+         }
+     }
+
+     @Override
+     protected void onDestroy() {
+         storiesProgressView.destroy();
+         super.onDestroy();
+     }
+
+     @Override
+     public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+         amTime.setText(getTimeProgressText(seekBarStart.getProgress()+1, true));
+         pmTime.setText(getTimeProgressText(seekBarEnd.getProgress()+1, false));
+     }
+
+     @Override
+     public void onStartTrackingTouch(SeekBar seekBar) { }
+
+     @Override
+     public void onStopTrackingTouch(SeekBar seekBar) { }
+
+     private String getTimeProgressText(int progress, boolean am){
+         if (am){
+             return progress + ":00 AM (" + progress + ":00)";
+         }
+         else{
+             return progress + ":00 PM (" + (progress + 12) + ":00)";
+         }
+     }
+ }
